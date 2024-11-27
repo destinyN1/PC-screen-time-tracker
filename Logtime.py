221e15a9083f7da41f6d0f51989182daa4cf6569
DestinyN1 <d.newman7@universityofgalway.ie>
2024-11-27 16:44:30 +0000

    implemented storage of values in dictionary, need to implement functionality to icrement time properly for each app

--- Logtime.py
+++ Logtime.py
@@ -1,5 +1,6 @@
 import time
 import win32gui
+import pprint
 
 
 #get active window
@@ -12,7 +13,7 @@ def get_active_window():
  return title if title.strip() else "Untitled Window" #handle empty titles
 
 current_app = get_active_window() #current app being used
-app_usage = {} #list that tracks app and app usage time
+app_usage = {} #dictionary that tracks app usage
 start_time = time.time() # start time of programme
 
 
@@ -21,10 +22,23 @@ start_time = time.time() # start time of programme
 while True:
   new_app = get_active_window()
   if new_app == current_app:
-     time_step = time.time()
-     elapsed_time = time_step - start_time
-     print(elapsed_time)
-     time.sleep(0.5)
+     time_step_curr = time.time()
+     elapsed_time_curr = time_step_curr - start_time
+     app_usage[current_app] = app_usage.setdefault(current_app,elapsed_time_curr)
+     print(f"elapsed time curr:\n")
+     pprint.pprint(app_usage)
+     time.sleep(1)
+     new_app = current_app
+
+  elif new_app != current_app:
+    time_step_new = time.time()
+    elapsed_time_new = time_step_new - start_time
+    print(f"elapsed time new: {elapsed_time_new}, {current_app}")
+    time.sleep(0.5)
+    current_app = new_app
+
+
+  
 
 
      
