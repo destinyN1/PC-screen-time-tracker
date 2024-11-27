cdaeee7c3bcd1632a70508deb1efa0c1a3977f36
DestinyN1 <d.newman7@universityofgalway.ie>
2024-11-27 17:21:05 +0000

    added some comments, next task is to get timer field to reset when app is switched

--- Logtime.py
+++ Logtime.py
@@ -23,16 +23,19 @@ while True:
   new_app = get_active_window()
   if new_app == current_app:
      
+     #calculating elapsed time for current app
      time_step_curr = time.time()
      elapsed_time_curr = time_step_curr - start_time
+#add new app to dictionary with elapsed time if it doesnt already exist and increment timer
+     app_usage[current_app] = app_usage.setdefault(current_app,elapsed_time_curr) 
+     app_usage[current_app] = elapsed_time_curr 
 
-     app_usage[current_app] = app_usage.setdefault(current_app,elapsed_time_curr)
-     app_usage[current_app] = elapsed_time_curr
+#print all the apps that have been running so far
+     print(f"Apps that have run:\n")
+     pprint.pprint(app_usage) 
 
-     print(f"elapsed time curr:\n")
-     pprint.pprint(app_usage)
-     
-     time.sleep(1)
+#suspend program for 500ms to not overload CPU
+     time.sleep(0.5)
      new_app = current_app
 
   elif new_app != current_app:
