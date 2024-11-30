9ee2187580923643b371244a2ed8491d916463d7
DestinyN1 <d.newman7@universityofgalway.ie>
2024-11-30 14:23:20 +0000

    split code into function blocks for easier integration

--- Logtime.py
+++ Logtime.py
@@ -4,6 +4,17 @@ import pprint
 
 
 
+
+
+def main():
+ current_app = get_active_window() #current app being used
+ app_usage = {} #dictionary that tracks app usage
+ start_time = time.time() # start time of programme
+
+ screentime(app_usage,start_time,current_app)
+
+
+
 #get active window
 def get_active_window():
  
@@ -17,40 +28,44 @@ def get_active_window():
    
 
 
-  
-current_app = get_active_window() #current app being used
-app_usage = {} #dictionary that tracks app usage
-start_time = time.time() # start time of programme
 
 
+def screentime(app_usage,start_time,current_app):
 
-
-while True:
-  new_app = get_active_window()
-  if new_app == current_app:
-     tic = time.perf_counter()
+ while True:
+   new_app = get_active_window()
+   if new_app == current_app:
      #calculating elapsed time for current app
-     elapsed_time_curr = time.time() - start_time
+      elapsed_time_curr = time.time() - start_time
+
 #add new app to dictionary with elapsed time if it doesnt already exist and increment timer
-     app_usage[current_app] = app_usage.setdefault(current_app,elapsed_time_curr) 
-     app_usage[current_app] = app_usage.get(current_app,0) + 1
+      app_usage[current_app] = app_usage.setdefault(current_app,elapsed_time_curr) 
+      app_usage[current_app] = app_usage.get(current_app,0) + 1
      
      
 #print all the apps that have been running so far
     
-     print(f"Apps that have run:\n")
-     pprint.pprint(app_usage) 
-     print("\n")
+      print(f"Apps that have run:\n")
+      pprint.pprint(app_usage) 
+      print("\n")
      
      
 
 #suspend program for 300ms to not overload CPU
-     time.sleep(1)
-     toc = time.perf_counter()
+      time.sleep(1)
  
 
 #app switching logic
-  elif new_app != current_app:
+   elif new_app != current_app:
     start_time = time.time()
     time.sleep(0.05)
     current_app = new_app
+
+ 
+   
+
+
+
+     
+if __name__ == "__main__":
+ main()
